03174ef980b0f8482b64f94a2b0569b4193f0508
wb-zhibeibei <13683501521@163.com>
2018-01-11 10:45:09 +0800

    microservice-simple-prvider-user

--- microservice-provider-user/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO USER (id, username, name, age, balance) VALUES (1, 'account1', '张三', 20, 100.00);
-INSERT INTO USER (id, username, name, age, balance) VALUES (2, 'account2', '李四', 28, 180.00);
-INSERT INTO USER (id, username, name, age, balance) VALUES (3, 'account3', '王五', 30, 200.00);
