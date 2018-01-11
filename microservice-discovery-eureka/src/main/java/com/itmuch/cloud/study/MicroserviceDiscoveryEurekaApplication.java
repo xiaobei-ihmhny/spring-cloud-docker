1ed76944026c0071508362c1ac1ef4831392003e
wb-zhibeibei <13683501521@163.com>
2018-01-11 14:10:12 +0800

    microservice-discovery-eureka

---
+++ microservice-discovery-eureka/src/main/java/com/itmuch/cloud/study/MicroserviceDiscoveryEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDiscoveryEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDiscoveryEurekaApplication.class, args);
+	}
+}
